d209784cae8dd0b2e2a3fd6b672bd96902f8bf8b
mohd mubeen <mohdmubeen5042@gmail.com>
2025-03-12 23:33:17 +0530

    initial commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://enabling-bluejay-10.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+"use client"
+export default function About(){
+    return (
+        <div>
+            about us page
+        </div>
+    )
+}
